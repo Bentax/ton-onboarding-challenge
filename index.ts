6e3911ee2b941b771c408779431bb7a2cd7265c9
Bentax <bentaxrus@mail.ru>
2023-06-01 16:20:13 +0300

    Added endpoint and TonClient

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-import {Address} from "ton"
+import {Address, TonClient} from "ton";
+import {getHttpEndpoint} from "@orbs-network/ton-access";
 
 async function main () {
 
-  const wallet = Address.parse('YOUR_WALLET_ADDRESS');
-  const collection = Address.parse('COLLECTION_ADDRESS');
+  const wallet = Address.parse('kQBqASax75JOWlfKz8aNuHnNtEld2RD8PE0t5ToZR0R_cQjo');
+  const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+
+  // get the decentralized RPC endpoint in Testnet
+  const endpoint = await getHttpEndpoint({
+    network: "testnet",
+  });
+
+  // initialize ton library
+  const client = new TonClient({ endpoint });
+
 
 }
 
