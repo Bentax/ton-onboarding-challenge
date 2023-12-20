4a00e022998bc60a805328cedba628648cf997d8
Bentax <bentaxrus@mail.ru>
2023-12-20 21:19:47 +0400

    some2

--- index.ts
+++ index.ts
@@ -9,8 +9,8 @@ const qrcode = require('qrcode-terminal');
 
 async function main () {
 
-  const wallet = Address.parse('kQBqASax75JOWlfKz8aNuHnNtEld2RD8PE0t5ToZR0R_cQjo');
-  const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+  const wallet = Address.parse('0QB6jUJDWPzkRMMltXgeBxAPRSYOlU5gaCohbEAiW-Mch0xD');
+  const collection = Address.parse('EQB6jUJDWPzkRMMltXgeBxAPRSYOlU5gaCohbEAiW-Mch6oM');
 
   // get the decentralized RPC endpoint in Testnet
   const endpoint = await getHttpEndpoint({
